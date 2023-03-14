b505c24323841b1286615540ac4cfb64f0590b3b
alejandro guzman <alejandro.guzman006@mymdc.net>
2023-03-14 08:32:08 -0400

    Fixing the pipe spawn

--- Assets/Programming/Script/SpawnPipe.cs
+++ Assets/Programming/Script/SpawnPipe.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 //Alejandro's script
 public class SpawnPipe : MonoBehaviour
 {
+    public GameObject[] brokenPipes;
     public GameObject prefab;       //The pipe that will be spawned
+    public bool isBroken;
 
     void Update()
     {
         //Will spawn a pipe when it finds a broken pipe in the scene
-        if (GameObject.FindGameObjectWithTag("BrokenPipe") && GameObject.FindGameObjectWithTag("ReplacePipe") == null)
+        for (int i = 0; i < brokenPipes.Length; i++)
         {
-            Instantiate(prefab, this.transform.position, Quaternion.identity);
+            if (brokenPipes[i].activeSelf && GameObject.FindGameObjectWithTag("ReplacePipe") == null)
+            {
+                Instantiate(prefab, this.transform.position, Quaternion.identity);
+            }
         }
     }
 }
