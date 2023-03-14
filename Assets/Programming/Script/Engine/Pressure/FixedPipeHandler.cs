dfeda28c3a8c84efe3d8af2775ebf8d985c7f501
JoshR918 <Sorajr.13@gmail.com>
2023-03-14 19:31:17 -0400

    Finally fixed the pipe functionality

--- Assets/Programming/Script/Engine/Pressure/FixedPipeHandler.cs
+++ Assets/Programming/Script/Engine/Pressure/FixedPipeHandler.cs
@@ -10,21 +10,34 @@ public class FixedPipeHandler : MonoBehaviour
     public GameObject[] audio;
     public GameObject[] normalPipe;  //Reference arrey to the NormalPipes GameObjects
     public GameObject[] brokenPipe;  //Reference arrey to the BrokenPipes GameObjects
+    public GameObject pype;
+    public GameObject pype2;
+    public GameObject pype3;
+    public GameObject pype4;
 
     //public bool normalPipeActive;    //Bool varibale to know the state of the normal pipes
     public bool brokenPipeActive;    //Bool varibale to know the state of the broken pipes
     public bool checkOnce = true;
 
     public bool brokenStatus;        //Bool Variblae to know when a pipe is broken
+    public bool normalPype;
+    public bool normalPype2;
+    public bool normalPype3;
+    public bool normalPype4;
+
 
     //Random
     public int random;               //Int varibale to hold the random number of the pipe that breaks
     public int total;                //Int varibale to know the Length of the arreys
 
     // Start is called before the first frame update
-    void Start()
+   public void Start()
     {
-       
+
+        normalPype = true;
+        normalPype2 = true;
+        normalPype3 = true;
+        normalPype4 = true;
 
         brokenPipeActive = false;    //Set to false becouse there is no pipe broken
         //normalPipeActive = true;     //Set to true becouse there is no pipe broken
@@ -32,7 +45,7 @@ public class FixedPipeHandler : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
         //Constantly get check the brokenPipe bool varibale in Pressure script in Parent
         brokenStatus = GetComponentInParent<Pressure>().brokenPipe;
@@ -42,6 +55,10 @@ public class FixedPipeHandler : MonoBehaviour
         {
             if (checkOnce == true)//Make sure it does the following code only once
             {
+               // normalPype2 = false;
+                //normalPype3 = false;
+                //normalPype4 = false;/
+
                 random = Random.Range(0, total);     //Set a random pipe to break
                 brokenPipe[random].SetActive(true);  //Set the correct broken pipe GameObject as true
                 audio[random].SetActive(true);
@@ -49,12 +66,84 @@ public class FixedPipeHandler : MonoBehaviour
                 normalPipe[random].SetActive(false); //Set the correct normal pipe GameObject as false
                 //normalPipeActive = false;            //Set to false becouse there is a broken pipe
                 checkOnce = false;//Make sure it does the following code only once
+               if(random == 0)
+                {
+
+                    normalPype = false;
+
+                }
+                if (random == 1)
+                {
+
+                    normalPype2 = false;
+
+                }
+                if (random == 2)
+                {
+
+                    normalPype3 = false;
+
+                }
+                if (random == 3)
+                {
+
+                    normalPype4 = false;
+
+                }
             }
+        }
+        if (normalPype == false)
+        {
+
+            pype.SetActive(false);
+
+        }
+        if (normalPype == true)
+        {
+
+            pype.SetActive(true);
+
+        }
+        if (normalPype2 == false)
+        {
+
+            pype2.SetActive(false);
+
+        }
+        if (normalPype2 == true)
+        {
+
+            pype2.SetActive(true);
+
+        }
+        if (normalPype3 == false)
+        {
+
+            pype3.SetActive(false);
+
+        }
+        if (normalPype3 == true)
+        {
+
+            pype3.SetActive(true);
+
+        }
+        if (normalPype4 == false)
+        {
+
+            pype4.SetActive(false);
+
+        }
+        if (normalPype4 == true)
+        {
+
+            pype4.SetActive(true);
+
         }
     }
 
     //On Collision Enter ...
-    void OnCollisionEnter(Collision other)
+    public void OnCollisionEnter(Collision other)
     {
         Debug.Log("Hii");
         //.. if brokenPipeActive bool varibale is fasle and the object on collision has the "ReplacePipe" tag
@@ -64,7 +153,34 @@ public class FixedPipeHandler : MonoBehaviour
             normalPipe[random].SetActive(true);                  //Set the correct normal pipe Game Object as true
             audio[random].SetActive(false);
             GetComponentInParent<Pressure>().brokenPipe = false; //Set the brokenPipe bool varibale in Pressure script in Parent as false
-            Destroy(other.gameObject);                           //Destroy the GameObject that was on collision
+
+            if (random == 0)
+            {
+
+                normalPype = true;
+
+            }
+            if (random == 1)
+            {
+
+                normalPype2 = true;
+
+            }
+            if (random == 2)
+            {
+
+                normalPype3 = true;
+
+            }
+            if (random == 3)
+            {
+
+                normalPype4 = true;
+
+            }
+        
+        Destroy(other.gameObject);
+            //Destroy the GameObject that was on collision
             /*foreach (GameObject obj in steamParticles)
             {
                 obj.SetActive(false);
