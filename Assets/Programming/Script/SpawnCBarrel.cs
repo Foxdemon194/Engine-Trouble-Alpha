a78ec031169852568e647a9d29094384b9038b08
alejandro guzman <alejandro.guzman006@mymdc.net>
2023-03-14 23:10:35 -0400

    Delaying the spawn of the coal barrels

--- Assets/Programming/Script/SpawnCBarrel.cs
+++ Assets/Programming/Script/SpawnCBarrel.cs
@@ -6,13 +6,21 @@ using UnityEngine;
 public class SpawnCBarrel : MonoBehaviour
 {
     public GameObject prefab;       //The coal barrel that will be spawned
+    public float spawnDelay = 3;        //How long the script will wait before spawning another barrel
+    float t;
 
     void Update()
     {
         //Will spawn a coal barrel if the one in the scene is destroyed
         if (GameObject.FindGameObjectWithTag("Barrel") == null)
         {
-            Instantiate(prefab, this.transform.position, Quaternion.identity);
+            t += Time.deltaTime;
+
+            if (t >= spawnDelay)
+            {
+                Instantiate(prefab, this.transform.position, Quaternion.identity);
+                t = 0;
+            }
         }
     }
 }
