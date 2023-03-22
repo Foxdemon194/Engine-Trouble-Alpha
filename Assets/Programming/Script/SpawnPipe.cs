e08eeea92fc448fc529e1ecf950a6052f771d79f
alejandro guzman <alejandro.guzman006@mymdc.net>
2023-03-22 17:23:19 -0400

    Changed the way the pipes spawn. They'll now check if there's already a replacement, and if there isn't, one will spawn after a set time. 3 pipes will be spawned at a time

--- Assets/Programming/Script/SpawnPipe.cs
+++ Assets/Programming/Script/SpawnPipe.cs
@@ -5,18 +5,50 @@ using UnityEngine;
 //Alejandro's script
 public class SpawnPipe : MonoBehaviour
 {
-    public GameObject[] brokenPipes;
-    public GameObject prefab;       //The pipe that will be spawned
-    public bool isBroken;
+    public Transform t1;
+    public Transform t2;
+    public Transform t3;
+    public GameObject prefab1;       //The pipe that will be spawned
+    public GameObject prefab2;
+    public GameObject prefab3;
+    public float spawnDelay = 3;        //How long the script will wait before spawning another barrel
+    float time1;
+    float time2;
+    float time3;
 
     void Update()
     {
         //Will spawn a pipe when it finds a broken pipe in the scene
-        for (int i = 0; i < brokenPipes.Length; i++)
+        if (GameObject.FindGameObjectWithTag("ReplacePipe1") == null)
         {
-            if (brokenPipes[i].activeSelf && GameObject.FindGameObjectWithTag("ReplacePipe") == null)
+            time1 += Time.deltaTime;
+
+            if (time1 >= spawnDelay)
             {
-                Instantiate(prefab, this.transform.position, Quaternion.identity);
+                Instantiate(prefab1, t1.position, Quaternion.identity);
+                time1 = 0;
+            }
+        }
+        //Will spawn a pipe when it finds a broken pipe in the scene
+        if (GameObject.FindGameObjectWithTag("ReplacePipe2") == null)
+        {
+            time2 += Time.deltaTime; 
+            
+            if (time2 >= spawnDelay)
+            {
+                Instantiate(prefab2, t2.position, Quaternion.identity);
+                time2 = 0;
+            }
+        }
+        //Will spawn a pipe when it finds a broken pipe in the scene
+        if (GameObject.FindGameObjectWithTag("ReplacePipe3") == null)
+        {
+            time3 += Time.deltaTime;
+
+            if (time3 >= spawnDelay)
+            {
+                Instantiate(prefab3, t3.position, Quaternion.identity);
+                time3 = 0;
             }
         }
     }
