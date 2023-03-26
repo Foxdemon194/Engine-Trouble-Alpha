44bc77ee43c516d3c069dc3324caab7f07aea48c
Ralfo Manzur <ra.manzurd@gmail.com>
2023-03-26 13:27:03 -0400

    small push, changes made to dialogue / script

--- Assets/Programming/Tutorial/BasicTutorialDialogue.cs
+++ Assets/Programming/Tutorial/BasicTutorialDialogue.cs
@@ -14,7 +14,7 @@ public class BasicTutorialDialogue : MonoBehaviour
     public int interval = 0;
     public float nextTime = 10;
     public int dialogueNumber;
-    public int DN = 0;
+    //public int DN = 0;
 
     public AudioSource playerAudioS;
     public AudioClip clip1;
@@ -41,9 +41,16 @@ public class BasicTutorialDialogue : MonoBehaviour
     public AudioClip clip22;
     public AudioClip status1;
     public AudioClip status2;
+    public AudioClip blank1;
+    public AudioClip blank2;
+    public AudioClip blank3;
+    public AudioClip blank4;
+    public AudioClip blank5;
+    public AudioClip blank6;
+    public AudioClip blank7;
 
     public bool cont;
-    public bool cont2;
+    //public bool cont2;
 
     // Start is called before the first frame update
     void Start()
@@ -89,131 +96,173 @@ public class BasicTutorialDialogue : MonoBehaviour
         }
         if (dialogueNumber == 4)
         {
-            interval = 10;
-            playerAudioS.clip = clip4;
+            interval = 3;
+            playerAudioS.clip = blank1;
             playerAudioS.Play();
         }
         if (dialogueNumber == 5)
+        {
+            interval = 5;
+            playerAudioS.clip = clip4;
+            playerAudioS.Play();
+        }
+        if (dialogueNumber == 6)
         {
             interval = 21;
             playerAudioS.clip = clip5;
             playerAudioS.Play();
             cont = false;
         }
-        if (dialogueNumber == 6)
+        if (dialogueNumber == 7)
         {
-            interval = 8;
+            interval = 3;
+            playerAudioS.clip = blank2;
+            playerAudioS.Play();
+        }
+        if (dialogueNumber == 8)
+        {
+            interval = 3;
             playerAudioS.clip = clip6;
             playerAudioS.Play();
         }
-        if (dialogueNumber == 7)
+        if (dialogueNumber == 9)
         {
             interval = 6;
             playerAudioS.clip = clip7;
             playerAudioS.Play();
         }
-        if (dialogueNumber == 8)
+        if (dialogueNumber == 10)
         {
             interval = 11;
             playerAudioS.clip = clip8;
             playerAudioS.Play();
         }
-        if (dialogueNumber == 9)
+        if (dialogueNumber == 11)
         {
             interval = 15;
             playerAudioS.clip = status1;
             playerAudioS.Play();
         }
-        if (dialogueNumber == 10)
+        if (dialogueNumber == 12)
         {
             interval = 11;
             playerAudioS.clip = status2;
             playerAudioS.Play();
         }
-        if (dialogueNumber == 11)
+        if (dialogueNumber == 13)
         {
             interval = 23;
             playerAudioS.clip = clip9;
             playerAudioS.Play();
             cont = false;
         }
-        if (dialogueNumber == 12)
+        if (dialogueNumber == 14)
         {
-            interval = 19;
+            interval = 3;
+            playerAudioS.clip = blank3;
+            playerAudioS.Play();
+        }
+        if (dialogueNumber == 15)
+        {
+            interval = 14;
             playerAudioS.clip = clip10;
             playerAudioS.Play();
         }
-        if (dialogueNumber == 13)
+        if (dialogueNumber == 16)
         {
-            interval = 16;
+            interval = 11;
             playerAudioS.clip = clip11;
             playerAudioS.Play();
         }
-        if (dialogueNumber == 14)
+        if (dialogueNumber == 17)
         {
             interval = 13;
             playerAudioS.clip = clip12;
             playerAudioS.Play();
             cont = false;
         }
-        if (dialogueNumber == 15)
+        if (dialogueNumber == 18)
         {
-            interval = 20;
+            interval = 3;
+            playerAudioS.clip = blank4;
+            playerAudioS.Play();
+        }
+        if (dialogueNumber == 19)
+        {
+            interval = 15;
             playerAudioS.clip = clip13;
             playerAudioS.Play();
         }
-        if (dialogueNumber == 16)
+        if (dialogueNumber == 20)
         {
-            interval = 13;
+            interval = 9;
             playerAudioS.clip = clip14;
             playerAudioS.Play();
         }
-        if (dialogueNumber == 17)
+        if (dialogueNumber == 21)
         {
             interval = 13;
             playerAudioS.clip = clip15;
             playerAudioS.Play();
             cont = false;
         }
-        if (dialogueNumber == 18)
+        if (dialogueNumber == 22)
         {
-            interval = 21;
+            interval = 3;
+            playerAudioS.clip = blank5;
+            playerAudioS.Play();
+        }
+        if (dialogueNumber == 23)
+        {
+            interval = 15;
             playerAudioS.clip = clip16;
             playerAudioS.Play();
         }
-        if (dialogueNumber == 19)
+        if (dialogueNumber == 24)
         {
             interval = 15;
             playerAudioS.clip = clip17;
             playerAudioS.Play();
         }
-        if (dialogueNumber == 20)
+        if (dialogueNumber == 25)
         {
             interval = 16;
             playerAudioS.clip = clip18;
             playerAudioS.Play();
             cont = false;
         }
-        if (dialogueNumber == 21)
+        if (dialogueNumber == 26)
         {
-            interval = 11;
+            interval = 3;
+            playerAudioS.clip = blank6;
+            playerAudioS.Play();
+        }
+        if (dialogueNumber == 27)
+        {
+            interval = 6;
             playerAudioS.clip = clip19;
             playerAudioS.Play();
             cont = false;
         }
-        if (dialogueNumber == 22)
+        if (dialogueNumber == 28)
         {
-            interval = 18;
+            interval = 3;
+            playerAudioS.clip = blank7;
+            playerAudioS.Play();
+        }
+        if (dialogueNumber == 29)
+        {
+            interval = 12;
             playerAudioS.clip = clip20;
             playerAudioS.Play();
         }
-        if (dialogueNumber == 23)
+        if (dialogueNumber == 30)
         {
             interval = 13;
             playerAudioS.clip = clip21;
             playerAudioS.Play();
         }
-        if (dialogueNumber == 24)
+        if (dialogueNumber == 31)
         {
             interval = 15;
             playerAudioS.clip = clip22;
